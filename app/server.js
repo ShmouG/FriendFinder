441645b5fcd31d02267f9395acf41d4ff00091a2
Mobious <sammiel.gross@gmail.com>
2018-10-04 23:31:13 -0500

    friends kinks

--- app/server.js
+++ app/server.js
@@ -5,6 +5,12 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+require("./routing/apiRoutes")(app);
+require("./routing/htmlRoutes")(app);
+
 app.listen(PORT, function() { 
     console.log(`listening on ${PORT}`)
 });
