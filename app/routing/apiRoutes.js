7a4a0ddce8decfc4b904039f184b136e900f9a86
Mobious <sammiel.gross@gmail.com>
2018-10-06 17:37:34 -0500

    getting there

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -7,6 +7,10 @@ const friends = require("../data/friends");
 
 module.exports = (app) => {
 
+    app.get('/api/friends', (req, res) => {
+		res.json(friends);
+	});
+
     app.post("/api/friends", (req, res) => {
         console.log(req.body.scores);
 
